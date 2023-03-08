6f92bc31b92c99f55570bdd75c0ca64fc2a1c372
Yuuki175 <realouise175@gmail.com>
2023-03-08 22:50:33 +0800

    1st commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# StarBucks
+# StarBucks
+# StarBucks
+# starbs
---
+++ script.js
@@ -0,0 +1,24 @@
+const kamote = document.querySelectorAll(".links__item h4");
+kamote.forEach((kamoteq) => {
+    kamoteq.addEventListener("click", () => {
+        kamoteq.nextElementSibling.classList.toggle("open");
+        kamoteq.querySelector("i").classList.toggle("open");
+    })
+})
+
+
+
+// const btnShow = document.querySelector('.btn__show');
+// const btnBack = document.querySelector('.btn__back');
+// const body = document.querySelector('body');
+
+// if(btnShow) {
+//     btnShow.addEventListener('click', () => {
+//         body.classList.add('show');
+//     })
+// }
+// if(btnBack) {
+//     btnBack.addEventListener('click', () => {
+//         body.classList.remove('show');
+//     })
+// }
