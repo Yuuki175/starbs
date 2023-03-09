6dd5f5f7559896b8e11b7148ec6c8f6c6548c69f
Louise Reña <realouise175@gmail.com>
2023-03-09 11:54:13 +0800

    2nd commit

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ kamote.forEach((kamoteq) => {
     })
 })
 
+const toggleMenu = document.querySelector(".toggle__menu");
+const headerNav = document.querySelector(".header__nav");
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("open");
+  headerNav.classList.toggle("open");
+});
 
 
 // const btnShow = document.querySelector('.btn__show');
